8b19f062eb5bd06adc80a12e55d21a95cc187b97
nyquil1x <gamboa.family.8@gmail.com>
2024-09-27 12:37:23 -0700

    fixed url

--- bearmessenger.rb
+++ bearmessenger.rb
@@ -1,7 +1,7 @@
 class Bearmessenger < Formula
   desc "Bear messaging app"
   homepage "https://github.com/vic-gamboa/BearMessenger"
-  url "https://github.com/vic-gamboa/BearMessenger/releases/download/v1.0/BearMessenger-v1.0.tar.gz"
+  url "https://github.com/vic-gamboa/BearMessenger/releases/download/v1.0.0/BearMessenger-v1.0.tar.gz"
   version "1.0"
   sha256 "8bb765b5ff7ab4528868ded354f47f613b12fede5df308aefb2e310ad80cc4ff"
 
