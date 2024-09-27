8f29e4525460bea41db34371bfdffc139cf9d2d2
nyquil1x <gamboa.family.8@gmail.com>
2024-09-27 12:31:34 -0700

    changed formula name

--- bearmessenger.rb
+++ bearmessenger.rb
@@ -1,11 +1,19 @@
 class Bearmessenger < Formula
   desc "Bear messaging app"
   homepage "https://github.com/vic-gamboa/BearMessenger"
-  url "https://github.com/vic-gamboa/BearMessenger/releases/download/v1.0.0/BearMessenger-v1.0.tar.gz"
+  url "https://github.com/vic-gamboa/BearMessenger/releases/download/v1.0/BearMessenger-v1.0.tar.gz"
   version "1.0"
-  sha256 "ec91c18e71e10287159ed56f49cd629a280235d80d6ad4ea5ed5ab7188e8f67f"
+  sha256 "8bb765b5ff7ab4528868ded354f47f613b12fede5df308aefb2e310ad80cc4ff"
+
+  depends_on "boost"  
+  depends_on "asio"   
 
   def install
+    system "make"
+    
+    bin.install "client_app/SimpleClient" => "client"
+    bin.install "server_app/SimpleServer" => "server"
+    
     bin.install "bear.sh" => "bearmessenger"
   end
 end
