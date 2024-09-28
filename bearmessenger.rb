891558b1e8d6cf1d4df3b241dd1e5ffc57d24e37
nyquil1x <gamboa.family.8@gmail.com>
2024-09-27 21:21:52 -0700

    bear

--- bearmessenger.rb
+++ bearmessenger.rb
@@ -1,9 +1,9 @@
 class Bearmessenger < Formula
   desc "Bear messaging app"
   homepage "https://github.com/vic-gamboa/BearMessenger"
-  url "https://github.com/vic-gamboa/BearMessenger/releases/download/v1.1.1/BearMessenger-v1.1.1.tar.gz"
-  version "1.1.1"
-  sha256 "1e7e8f05769be17cfa11c18945865de2eaec140dcc18945cff87a294e2134fa7"
+  url "https://github.com/vic-gamboa/BearMessenger/releases/download/v1.1.2/BearMessenger-v1.1.2.tar.gz"
+  version "1.1.2"
+  sha256 "177c890970a0b2b524a04995553133dbe3673774a24167470bb79b01083e306e"
 
   depends_on "boost"
   depends_on "asio"
